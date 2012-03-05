5829edc9bcc7f0904af6fd989e8c9019bae18eba
maniacbug <maniacbug@ymail.com>
2012-03-04 18:43:19 -0800

    Remove prog_char

--- uip_log.h
+++ uip_log.h
@@ -2,6 +2,6 @@
 #define __UIP_LOG_H__
 #include <avr/pgmspace.h>
 
-extern "C" void uip_log_P(prog_char* msg);
+extern "C" void uip_log_P(const char* msg);
 extern "C" void uip_log(char* msg);
 #endif // __UIP_LOG_H__
